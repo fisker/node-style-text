509e362940eb12dd62ca29202b3e764a52be3233
fisker <lionkay@gmail.com>
2025-01-22 17:37:52 +0800

    Reduce package size

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import util from 'node:util'
 
-const factory = (formats = []) =>
+const factory = (...formats) =>
   new Proxy(util.styleText.bind(util, formats), {
     get: (target, property) =>
-      util.inspect.colors[property] && factory([...formats, property]),
+      util.inspect.colors[property] && factory(...formats, property),
   })
 
 export default factory()
