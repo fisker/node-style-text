6d16c563e6b988390fe954d7e0fcba4845b6d177
fisker <lionkay@gmail.com>
2025-05-11 16:51:10 +0800

    Simplify

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 import util from 'node:util'
 import process from 'node:process'
 
-const createColors = (options) => {
-  const factory = (...formats) =>
-    new Proxy((text) => util.styleText(formats, text, options), {
-      get: (_, format) =>
-        util.inspect.colors[format] && factory(...formats, format),
-    })
-  return factory()
-}
+const factory = (options, ...formats) =>
+  new Proxy((text) => util.styleText(formats, text, options), {
+    get: (_, format) =>
+      util.inspect.colors[format] && factory(options, ...formats, format),
+  })
 
-export default createColors()
-export const styleTextStderr = createColors({stream: process.stderr})
+export default factory()
+export const styleTextStderr = factory({stream: process.stderr})
