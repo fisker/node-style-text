070c597cb9a20e5fa88891dcc79b5fe2f5d9fd9f
fisker <lionkay@gmail.com>
2025-05-11 17:05:41 +0800

    Restore

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 import util from 'node:util'
-import process from 'node:process'
 
-const factory = (options, ...formats) =>
-  new Proxy((text) => util.styleText(formats, text, options), {
+const factory = (...formats) =>
+  new Proxy(util.styleText.bind(util, formats), {
     get: (_, format) =>
-      util.inspect.colors[format] && factory(options, ...formats, format),
+      util.inspect.colors[format] && factory(...formats, format),
   })
 
 export default factory()
-export const styleTextStderr = factory({stream: process.stderr})
+export const styleTextStderr = factory()
