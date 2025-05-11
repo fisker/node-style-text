ac4811ddaee1675538ef945b906ca3d21660c317
fisker <lionkay@gmail.com>
2025-05-11 17:16:14 +0800

    arrow function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import util from 'node:util'
 
 const factory = (...formats) =>
-  new Proxy(util.styleText.bind(util, formats), {
+  new Proxy((text) => util.styleText(formats, text), {
     get: (_, format) =>
       util.inspect.colors[format] && factory(...formats, format),
   })
