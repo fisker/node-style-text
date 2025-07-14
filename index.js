c1e3b401d14b22eaf3671b5470d3633abbe7a0c2
fisker <lionkay@gmail.com>
2025-07-14 23:57:04 +0800

    Support `none`

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ import util from 'node:util'
 
 const factory = (...formats) =>
   new Proxy(util.styleText.bind(util, formats), {
-    get: (_, format) =>
-      util.inspect.colors[format] && factory(...formats, format),
+    get: (_, format) => factory(...formats, format),
   })
 
 export default factory()
